d981f602efb187d16e5668590d7fa6d18b19e4c4
Brad Davis <brad@transparentproperties.com>
2018-03-11 16:56:56 -0700

    adding ENV vars config and civic NPM deps needed

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
+
+console.log(process.env);
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.SERVER_PORT || 8080;
+const HOST = process.env.SERVER_HOST|| '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
     res.send('civic-proxy\n');
 });
+
+
+
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
