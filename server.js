979619a72183a54ddf34417e945e6ad2cca1a5fc
Brad Davis <brad@transparentproperties.com>
2018-03-11 23:23:20 -0700

    civic-proxy working with platform.  handling CORS and body JSON body parsing.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 console.log(process.env);
@@ -12,6 +13,15 @@ const HOST = process.env.SERVER_HOST|| '0.0.0.0';
 // App
 const app = express();
 
+// for parsing application/json
+app.use(bodyParser.json());
+
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var routes = {
     civic: require( './lib/routes/civic' ),
